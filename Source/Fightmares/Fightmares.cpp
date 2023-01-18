7c25e8804fdbfc4065c541ae09630ace38ea5557
Jamie Carnell <jc228467@falmouth.ac.uk>
2023-01-18 22:43:32 +0000

    Uploaded funne unreal project

---
+++ Source/Fightmares/Fightmares.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Fightmares.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Fightmares, "Fightmares" );
